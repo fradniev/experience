2204fa0f675e75ab9f0fc4b901d43a00a1fdb3eb
fradniev <fradniev@hotmail.com>
2017-12-16 07:42:28 -0400

    function to move blocks ready

--- js/main.js
+++ js/main.js
@@ -2,33 +2,32 @@ jQuery(document).ready(function($) {
 	function move_block(x,y,target){
 		var side;
 		if (x>0 || x<0) {
-			$(target).css("left",x+"px");
+			$("#"+target).css("left",x+"px");
 		}
 		if(y>0 || y<0){
-			$(target).css("top",y+"px");
+			$("#"+target).css("top",y+"px");
 		}
-		if($(target).hasClass('info-box-left')){
+		if($("#"+target).hasClass('info-box-left')){
 			side=" .left-line";
 		}
 		else{
 			side=" .right-line";
 		}
-		if(($(".experience .mid-line").offset().left- $(target).offset().left)>=0){
-			if($(target+side).css('transform') != null){
-				$(target+side).css({
+		if(($(".experience .mid-line").offset().left- $("#"+target).offset().left)>=0){
+			if($("#"+target+side).css('transform') != null){
+				$("#"+target+side).css({
 					"transform": "unset"
 				});
 			}
-			$(target+side).css("width",($(".experience .mid-line").offset().left- $(target).offset().left)+"px");
+			$("#"+target+side).css("width",($(".experience .mid-line").offset().left- $("#"+target).offset().left)+"px");
 		}
 		else{
-			if($(target+side).css('transform') == null){
-				$(target+side).css({
+			if($("#"+target+side).css('transform') == "none"){
+				$("#"+target+side).css({
 					"transform": "rotate(180deg)",
-					"transform-origin": "top left"
 				});
 			}
-			$(target+side).css("width",(($(".experience .mid-line").offset().left- $(target).offset().left)*-1)+"px");
+			$("#"+target+side).css("width",(($(".experience .mid-line").offset().left- $("#"+target).offset().left)*-1)+"px");
 		}
 		return true;
 	}
